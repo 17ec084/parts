f07fceab800e73ce65324aa9bce32bb40ec384bc
Tomotaka_Hirata <17ec084@ms.dendai.ac.jp>
2019-04-09 22:02:18 +0900

    Update ruby.php
    とりあえず動作の確認はできた。

--- php/ruby/ruby.php
+++ php/ruby/ruby.php
@@ -11,33 +11,33 @@ include "include/str_replace_once.php";
 
 $ready=isset($_GET['text']) && isset($_GET['ruby']);
 
-$rubyReady=$_GET['ruby']=='false' || $_GET['ruby']=='true'
+$rubyReady=$_GET['ruby']=='false' || $_GET['ruby']=='true';
 
-$ready=$ready && $rubyReady;
+$ready=($ready && $rubyReady);
 
 if($ready)
 {
     $text=$_GET['text'];
 
-    if($_GET['ruby']=='false')
+    if(strpos($text,'<hirata type="ruby">')!==false)
     {
-        $text=rubyCut($text);//未実装
+        $text=formatChangeRuby($text);
+    }
+
+    if($_GET['ruby']!='true')
+    {
+        $text=rubyCut($text);
     }
     print $text;
 }
 else
 {
-    print 'error: textとrubyをGET送信してください。<br>rubyはfalseまたはtrueを値としてください。<br>サポートは<a href="https://github.com/17ec084/parts/tree/master/php/ruby">こちら</a>。'
+    print 'error: textとrubyをGET送信してください。<br>rubyはfalseまたはtrueを値としてください。<br>サポートは<a href="https://github.com/17ec084/parts/tree/master/php/ruby">こちら</a>。';
 }
 
 
     function rubyCut($text)
     {
-        if(strpos($text,'<hirata type="ruby">')!==false)
-        {
-            $text=formatChangeRuby($text);//未実装
-        }
-
         $text=str_replace('<ruby>','',$text);
         $text=str_replace('</ruby>','',$text);
         $text=str_replace('<rb>','',$text);
@@ -49,47 +49,50 @@ else
     }
 
 
-        function formatChangeRuby($text)
+    function formatChangeRuby($text)
+    {
+        while(strpos($text,'<hirata type="ruby">')!==false)
         {
-            while(strpos($text,'<hirata type="ruby">')!==false)
+            $hirataText=getTextFromTo($text,'<hirata type="ruby">','</hirata>',false);
+            //最初の<hirata>タグ内の文字列を抽出
+
+            while(strpos($hirataText, '[')!==false)
             {
-                $hirataText=getTextFromTo($text,'<hirata type="ruby">','</hirata>',true);
-                //最初の<hirata>タグ内の文字列を抽出
+                $kanji=getTextFromTo($hirataText,'[','|',true);
+                $kanji=rtrim($kanji, '|');
+                //現在$kanjiの中身の例は
+                //"[文字"
 
-                while(strpos($hirataText, '[')!==false)
-                {
-                    $kanji=getTextFromTo($hirataText,'[','|',true);
-                    $kanji=rtrim($kanji, '|');
-                    //現在$kanjiの中身の例は
-                    //"[文字"
-
-                    $replace=ltrim($kanji, '[');
-                    //"文字"
-                    $replace=
+                $replace=ltrim($kanji, '[');
+                //"文字"
+                $replace=
 
 '<ruby>
     <rb>'.$replace.'</rb>
     <rp>(</rp>
         <rt>';
 
-                    $hirataText = str_replace_once($kanji, $replace, $hirataText, 0);
+                $hirataText = str_replace_once($kanji, $replace, $hirataText, 0);
 
-                    $yomigana=getTextFromTo($hirataText,'|',']',true);
-                    $yomigana=ltrim($yomigana, '|');
-                    //現在$yomiganaの中身の例は
-                    //"もじ]"
+                $yomigana=getTextFromTo($hirataText,'|',']',true);
+                //現在$yomiganaの中身の例は
+                //"|もじ]"
 
-                    $replace=rtrim($yomigana, ']');
-                    //"もじ"
-                    $replace=
+                $replace=rtrim($yomigana, ']');
+                $replace=ltrim($replace, '|');
+                //"もじ"
+                $replace=
 
             $replace.'</rt>
     <rp>)</rp>
 </ruby>';
-                    $hirataText = str_replace_once($yomigana, $replace, $hirataText, 0);
-                }   
+                $hirataText = str_replace_once($yomigana, $replace, $hirataText, 0);
+            }   
 
-                $text = str_replace_once(
+            $text = $hirataText;
 
-            }
         }
+
+        return $text;
+    
+    }
